b4f7c1b25f29fbdfd8c93d1862409b527ac3998b
PankajKrModi <forgetnot.1993@gmail.com>
2018-06-26 09:51:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  emp_list=[
+    {"name":"John","pos":"Manager"},
+    {"name":"Sam","pos":"Del Manager"}
+  ];
+  modal:any={};
+  addEmp(){
+    if(this.modal!=null)
+     this.emp_list.push(this.modal);
+    this.modal={};
+    }
+  
+  delete(i){
+    this.emp_list.splice(i,1);
+  }
+
+
+  myValue:number;
+  modal2:any={};
+  edit(k:number){ 
+    this.myValue=k;
+    this.modal2.name=this.emp_list[k].name;
+    this.modal2.pos=this.emp_list[k].pos;
+  }
+  save(){
+    let k=this.myValue;
+    this.emp_list[k] = this.modal2;
+    this.modal2={};
+  }
 }
